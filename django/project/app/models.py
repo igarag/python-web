0962286ee7baecfa9da35c6585a9d92be55f108d
igarag <n.arranz.agueda@gmail.com>
2021-03-30 22:50:41 +0200

    user model

--- django/project/app/models.py
+++ django/project/app/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, username, email, password=None, first_name=None, last_name=None):
+        if not username:
+            raise TypeError('Users should have a username')
+        if not email:
+            raise TypeError('Users should have a Email')
+
+        user = self.model(username=username,
+                          email=self.normalize_email(email),
+                          first_name=first_name,
+                          last_name=last_name)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, username, email, password=None):
+        if password is None:
+            raise TypeError('Password should not be none')
+
+        user = self.model(username=username, email=self.normalize_email(email))
+        user.set_password(password)
+        user.is_superuser = True
+        user.is_active = True
+        user.is_staff = True
+        user.save()
+        return user
