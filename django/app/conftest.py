d8bcd601f704ea1b3391ce03ca2daa2b0df21a8f
igarag <n.arranz.agueda@gmail.com>
2021-10-18 23:10:31 +0200

    :sparkles: Add TDD files

--- django/app/conftest.py
+++ django/app/conftest.py
@@ -1,20 +1,21 @@
-import os
-
+from environs import Env
 from django.conf import settings
 
 import pytest
 
+env = Env()
+env.read_env()
 
-DEFAULT_ENGINE = "django.db.backends.postgresql_psycopg2"
+DEFAULT_ENGINE = "django.db.backends.postgresql"
 
-
-@pytest.fixture(scope="session")
-def django_db_setup():
-    settings.DATABASES["default"] = {
-        "ENGINE": os.environ.get("DB_TEST_ENGINE", DEFAULT_ENGINE),
-        "HOST": os.environ["DB_TEST_HOST"],
-        "NAME": os.environ["DB_TEST_NAME"],
-        "PORT": os.environ["DB_TEST_PORT"],
-        "USER": os.environ["DB_TEST_USER"],
-        "PASSWORD": os.environ["DB_TEST_PASSWORD"],
-    }
+#
+# @pytest.fixture(scope="session")
+# def django_db_setup():
+#     settings.DATABASES["default"] = {
+#         "ENGINE": env.str("DB_TEST_ENGINE", DEFAULT_ENGINE),
+#         "HOST": env.str("DB_TEST_HOST", ""),
+#         "NAME": env.str("DB_TEST_NAME", ""),
+#         "PORT": env.str("DB_TEST_PORT", ""),
+#         "USER": env.str("DB_TEST_USER", ""),
+#         "PASSWORD": env.str("DB_TEST_PASSWORD", ""),
+#     }
